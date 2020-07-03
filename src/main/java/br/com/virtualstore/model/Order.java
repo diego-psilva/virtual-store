7d042730df204f14cf82feadced51f4b80791ab2
Diego PS <diegops.tads@gmail.com>
2020-07-03 13:07:01 -0300

    completed entity

---
+++ src/main/java/br/com/virtualstore/model/Order.java
@@ -0,0 +1,49 @@
+package br.com.virtualstore.model;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+import br.com.virtualstore.model.payment.Payment;
+
+public class Order {
+	private final int id;
+    private final Client client;
+    private final LocalDateTime dateTime;
+    private final List<ProductHolder> productHolders;
+    private final Payment payment;
+
+    public Order(int id, Client client, List<ProductHolder> productHolders, Payment payment) {
+        this.id = id;
+        this.client = client;
+        dateTime = LocalDateTime.now();
+        this.productHolders = productHolders;
+        this.payment = payment;
+    }
+
+    public int getClientId() {
+        return client.getId();
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public List<ProductHolder> getProductHolders() {
+        return Collections.unmodifiableList(productHolders);
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Integer) {
+            return id==(int) obj;
+        }
+        return super.equals(obj);
+    }
+}
+
+
